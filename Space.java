223263aa8bcab1f3e8c74b6708de2518115d985e
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-07 10:27:27 +0300

    reformat code

--- Space.java
+++ Space.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Space {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your sentence");
         String mess = sc.nextLine();
         int space = 0;
-        for(int i = 0; i<mess.length();i++){
-            if(mess.charAt(i)==' '){
+        for (int i = 0; i < mess.length(); i++) {
+            if (mess.charAt(i) == ' ') {
                 space++;
             }
         }
-        System.out.println("The number of spaces is:"+space);
+        System.out.println("The number of spaces is:" + space);
     }
 }
