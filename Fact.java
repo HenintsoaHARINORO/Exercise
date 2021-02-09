69682f00afd5a039050055bc5def324845342c24
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-09 16:41:51 +0300

    changes

--- Fact.java
+++ Fact.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
 public class Fact {
-     static int Factoriel(int u){
-         if(u==1)
-             return 1;
-          else
-              return u*Factoriel(u-1);
+    static int Factoriel(int u) {
+        if (u == 1)
+            return 1;
+        else
+            return u * Factoriel(u - 1);
     }
-    public static  void main(String [] args){
-         int res;
-         System.out.println("Enter a number: ");
+
+    public static void main(String[] args) {
+        int res;
+        System.out.println("Enter a number: ");
         Scanner sc = new Scanner(System.in);
-       int a= sc.nextInt();
-       res=Factoriel(a);
-       System.out.println(a+"! ="+res);
+        int a = sc.nextInt();
+        res = Factoriel(a);
+        System.out.println(a + "! =" + res);
     }
 }
