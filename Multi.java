b2ba8ef1b2ae6432766e51459679dbe98b9cdd29
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-09 16:22:23 +0300

    reformated code

--- Multi.java
+++ Multi.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 public class Multi {
     static int multi(int u) {
         int i = 0;
-        int res=0;
+        int res = 0;
         for (; i < 11; i++) {
-             res = i * u;
-            System.out.println(i + " " + "*"+u + "=" + res);
+            res = i * u;
+            System.out.println(i + " " + "* " + u + " = " + res);
         }
 
         return res;
     }
-    public static void main(String [] args){
+
+    public static void main(String[] args) {
         System.out.println("Please enter a number: ");
         Scanner sc = new Scanner(System.in);
-     int a=sc.nextInt();
-     int res=0;
-     res=multi(a);
+        int a = sc.nextInt();
+        int res = 0;
+        res = multi(a);
 
     }
 }
