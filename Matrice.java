3d5b412bbcf42e91e92525e58a8be3464057da9b
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-10 16:59:52 +0300

    reformated code

--- Matrice.java
+++ Matrice.java
@@ -1,13 +1,14 @@
 public class Matrice {
-    static void display(int[] ...u){
-        for (int [] k:u){
-            for (int v:k)
-                System.out.print(v+" ");
+    static void display(int[]... u) {
+        for (int[] k : u) {
+            for (int v : k)
+                System.out.print(v + " ");
             System.out.println();
         }
     }
-    public static void main(String [] arg){
-        int [][] mat={{1,2,3,4},{5,6,7,8},{12,23,24,25}};
+
+    public static void main(String[] arg) {
+        int[][] mat = {{1, 2, 3, 4}, {5, 6, 7, 8}, {12, 23, 24, 25}};
         display(mat);
     }
 }
