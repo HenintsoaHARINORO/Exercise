75c3dd00f0ff1ce3a753c34e418d369471dc6a3c
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-10 16:54:35 +0300

    matrix

---
+++ Matrice.java
@@ -0,0 +1,13 @@
+public class Matrice {
+    static void afficher(int[] ...u){
+        for (int [] k:u){
+            for (int v:k)
+                System.out.print(v+" ");
+            System.out.println();
+        }
+    }
+    public static void main(String [] arg){
+        int [][] mat={{1,2,3,4},{5,6,7,8},{12,23,24,25}};
+        afficher(mat);
+    }
+}
