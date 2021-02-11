3e8ac0bf650aeaf7e7014edf339de94ad2438e9a
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-11 19:38:11 +0300

    reformated code

--- Char.java
+++ Char.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
 public class Char {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your sentence");
         String mess = sc.nextLine();
         System.out.println("Please enter the character you want to check:");
-        String x= sc.nextLine();
-        int n= mess.indexOf(x);
+        String x = sc.nextLine();
+        int n = mess.indexOf(x);
 
-        if( n !=-1){
-            System.out.println(mess + " contains "+ x);
-        }
-        else
+        if (n != -1) {
+            System.out.println(mess + " contains " + x);
+        } else
             System.out.println(mess + "does not contain" + x);
 
     }
