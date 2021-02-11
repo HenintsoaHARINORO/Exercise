faa6212892e7efb3872b85f15b6d24cf62c68f46
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-11 19:35:57 +0300

    check not count

--- Char.java
+++ Char.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 public class Char {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Veuillez entrer le message");
+        System.out.println("Please enter your sentence");
         String mess = sc.nextLine();
-        int n= mess.indexOf('x');
+        System.out.println("Please enter the character you want to check:");
+        String x= sc.nextLine();
+        int n= mess.indexOf(x);
 
         if( n !=-1){
-            System.out.println(mess + " contient le caractere 'x'");
+            System.out.println(mess + " contains "+ x);
         }
         else
-            System.out.println(mess + " ne contient pas le caractere 'x'");
+            System.out.println(mess + "does not contain" + x);
 
     }
 }
