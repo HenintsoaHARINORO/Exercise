38749652fed6ee3b17b9bba44b1aaab6ce26ce9f
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-12 18:18:05 +0300

    Removed boxing

--- Conversion3.java
+++ Conversion3.java
@@ -1,23 +1,23 @@
 public class Conversion3 {
     public static void main(String [] args){
         int a = 1234;
-        Integer A = new Integer(a);
+        Integer A = a;
         String strA= A.toString();
 
         System.out.println(strA);
 
         float b =1234.56f;
-        Float B = new Float(b);
+        Float B =b;
         String strB = B.toString();
         System.out.println(strB);
 
         double c = 123456.789d;
-        Double C = new Double(c);
+        Double C = c;
         String strC= C.toString();
         System.out.println(strC);
 
         byte d = 113;
-        Byte D= new Byte(d);
+        Byte D= d;
         String strD = D.toString();
         System.out.println(strD);
     }
