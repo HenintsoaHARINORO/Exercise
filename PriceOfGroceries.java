203c35091ef1f389dc2225004b3575c6456c2123
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-03-04 18:08:18 +0300

    algorithms

---
+++ PriceOfGroceries.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PriceOfGroceries {
+    public static void main(String [] args){
+        Map<String,Double> book= new HashMap<>();
+        book.put("apple",0.67);
+        book.put("milk",1.49);
+        book.put("avocado",1.49);
+        System.out.println(book);
+    }
+}
