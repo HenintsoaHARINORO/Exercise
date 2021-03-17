af95dfa93e7ff39c1aef625275ed563706d045e9
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-03-17 12:53:05 +0300

    Tic Tac Toe

---
+++ Tic Tac Toe/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try {
+            BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("sample.fxml"));
+            Scene scene = new Scene(root,300,320);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
